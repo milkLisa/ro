81267a65d8502be9ecb542d3f64e8e2fd26b97ec
lisa <lisa231645@gmail.com>
2023-09-24 20:03:23 +0800

    Re-download monster images from nn.ai4rei.net

--- py_scripts/crawl_to_csv.py
+++ py_scripts/crawl_to_csv.py
@@ -39,7 +39,8 @@ print("Start crawling monsters from rd.fharr.com at {}".format(start_time))
 ================= load monster list ==================
 """
 moblist_url = "https://rd.fharr.com/moblist"
-data = requests.get(moblist_url).text
+session = requests.session()
+data = session.get(moblist_url).text
 soup = BeautifulSoup(data, "html.parser")
 
 category_name = {"boss": "BOSS魔物", "mvp": "MVP魔物"}
@@ -49,11 +50,11 @@ for name in category_name :
   print("Load {}...from {}".format(name, category.attrs["href"]))
 
   page = 1
-  last_page = 10
+  last_page = 1
   while page <= last_page :
     print("page {}".format(page))
     url = "{}&g=1&page={}".format(category.attrs["href"], page)
-    data = requests.get(url).text
+    data = session.get(url).text
     monsters = concat_monster(monsters, data)
 
     if page == 1 :
@@ -77,16 +78,26 @@ respawnData = pd.DataFrame()
 for row in monsters.itertuples(index=False) :
   print("Fatch {}'s image and respawn data".format(row.ID))
   url = "https://rd.fharr.com/db/monster/{}".format(row.ID)
-  data = requests.get(url).text
-  soup = BeautifulSoup(data, "html.parser")
+  
+  #動態載入js可能造成img url為空, 等待1秒後重新請求
+  reqCount = 1
+  stopCount = 3
+  while reqCount < stopCount :
+    data = session.get(url).text
+    soup = BeautifulSoup(data, "html.parser")
+    reqCount += 1
 
-  #get image url
-  imgUrl = ""
-  images = soup.select("img.img-radius")
-  if len(images) > 0 :
-    imgUrl = images[0].get("src")
-    tmpData = pd.DataFrame({"id": [row.ID], "img": [imgUrl]})
-    imgData = pd.concat([imgData, tmpData], ignore_index=True)
+    #get image url
+    imgUrl = ""
+    images = soup.select("img.img-radius")
+    if len(images) > 0 :
+      imgUrl = images[0].get("src")
+      tmpData = pd.DataFrame({"id": [row.ID], "img": [imgUrl]})
+      imgData = pd.concat([imgData, tmpData], ignore_index=True)
+      reqCount = stopCount
+    elif reqCount == 2 :
+      print("Can't not get {} img url, try again after 1 second".format(row.ID))
+      sleep(1)
 
   #get respawn data
   location = soup.find(string=lambda text: text=="重生時間")
@@ -98,7 +109,7 @@ for row in monsters.itertuples(index=False) :
     for email in encodeEmails :
       cfemail = email.get("data-cfemail")
       table.find(attrs={"data-cfemail": cfemail}).replace_with(decodeEmail(cfemail))
-      
+    
     data = pd.read_html(str(table))[0]
 
     #remove tfoot
@@ -113,7 +124,7 @@ for row in monsters.itertuples(index=False) :
     #add id column
     data = data.assign(id=row.ID)
     respawnData = pd.concat([respawnData, data], ignore_index=True)
-  
+
   sleep(1)
 
 print("Catch {} image url".format(len(imgData)))
--- py_scripts/download_image.py
+++ py_scripts/download_image.py
@@ -1,10 +1,12 @@
 """
-從monsters.csv的img url下載圖檔,
+對照images.csv從nn.ai4rei.net下載圖檔,
+若失敗則從monsters.csv的img url下載,
 並縮小成max 64*64 pixel,
 再轉存png格式至public/static/images資料夾
 Updated: 2023/09/24
 """
 from time import sleep
+from bs4 import BeautifulSoup
 from PIL import Image
 import pandas as pd
 import requests
@@ -12,62 +14,98 @@ import datetime
 import os
 import io
 
+def reduce_and_save_image(content, outpath) :
+  """
+  with open(outpath, "wb") as outfile:
+    outfile.write(content)
+  """
+
+  ###### 用Pillow處理圖片才存檔 ######
+  image = Image.open(io.BytesIO(content))
+  imgWidth, imgHeight = image.size
+  print("image size: {} {}".format(imgWidth, imgHeight))
+
+  # 等比例縮放
+  MAXWIDTH, MAXHEIGHT = (64, 64)
+  if (imgWidth > imgHeight) & (imgWidth != MAXWIDTH) :
+    imgHeight = round(imgHeight * MAXWIDTH / imgWidth)
+    imgWidth = MAXWIDTH
+  elif (imgHeight > imgWidth) & (imgHeight != MAXHEIGHT) :
+    imgWidth = round(imgWidth * MAXHEIGHT / imgHeight)
+    imgHeight = MAXHEIGHT
+  
+  image = image.resize((imgWidth, imgHeight), Image.BILINEAR)
+  print("after resize: {} {}".format(imgWidth, imgHeight))
+
+  """
+  # 裁切長型圖片
+  if imgHeight > imgWidth :
+    image = image.crop((0, 0, imgWidth, imgWidth))  # (left, upper, right, lower)
+    imgWidth, imgHeight = image.size
+    print("after crop: {} {}".format(imgWidth, imgHeight))
+  """
+
+  # Reducing Quality
+  #quality_val = 90
+
+  #image.show()
+  image.save(outpath, "png")
+
+"""
+================= load images list ==================
+"""
 monsters = pd.read_csv("monsters.csv", encoding="utf-8")
 monsters = monsters.filter(items=["ID", "img"], axis=1)
 monsters = monsters.drop_duplicates()
 
 images = pd.read_csv("images.csv", encoding="utf-8")
-images.columns = ["ID", "image"]
-images = images.join(monsters.set_index("ID"), on="ID")
+images = images.join(monsters.set_index("ID"), on="roId")
 
 start_time = datetime.datetime.now()
 print("Ready to download {} monsters image...{}".format(len(images), start_time))
 
+"""
+================= download images ==================
+"""
+dest_dir = "../public/static/images/"
+headers = {
+    "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding" : "gzip, deflate, br",
+    "Accept-Language" : "zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Connection" : "keep-alive",
+    "DNT" : "1",
+    "Host" : "nn.ai4rei.net",
+    "Sec-Fetch-Dest" : "document",
+    "Sec-Fetch-Mode" : "navigate",
+    "Sec-Fetch-Site" : "none",
+    "Sec-Fetch-User" : "?1",
+    "Upgrade-Insecure-Requests" : "1",
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"
+    }
+session = requests.session()
+host = "https://nn.ai4rei.net/dev/npclist/"
+data = session.get("{}?qq=all".format(host)).text
+soup = BeautifulSoup(data, "html.parser")
+
+failed_list = pd.DataFrame()
 for row in images.itertuples(index=False) :
-  outpath = os.path.join("../public/static/images/", row.image)
-
+  outpath = os.path.join(dest_dir, row.image)
+  
   if os.path.isfile(outpath) :
-    print("{}'s image exist".format(row.ID))
+    print("{}'s image exist".format(row.roId))
   else :
-    print("Download {}'s image, {}".format(row.ID, row.img))
-    
-    image = requests.get(row.img)
-    """
-    with open(outpath, "wb") as outfile:
-      outfile.write(image.content)
-    """
+    item = soup.select("#tagme_{}".format(row.roId))
 
-    ###### 用Pillow處理圖片才存檔 ######
-    image = Image.open(io.BytesIO(image.content))
-    imgWidth, imgHeight = image.size
-    print("image size: {} {}".format(imgWidth, imgHeight))
+    if len(item) > 0 :
+      img_url = "{}{}".format(host, item[0].find_previous_sibling("img").attrs["src"])
+      print("Download {}'s image, {}".format(row.roId, img_url))
+      res = session.get(img_url, headers=headers)
+    else :
+      print("Failed, try to download from rd.fharr.com".format(row.roId))
+      print("Download {}'s image, {}".format(row.roId, row.img))
+      res = session.get(row.img)
 
-    # 等比例縮放
-    MAXWIDTH, MAXHEIGHT = (64, 64)
-    if (imgWidth > imgHeight) & (imgWidth != MAXWIDTH) :
-      imgHeight = round(imgHeight * MAXWIDTH / imgWidth)
-      imgWidth = MAXWIDTH
-    elif (imgHeight > imgWidth) & (imgHeight != MAXHEIGHT) :
-      imgWidth = round(imgWidth * MAXHEIGHT / imgHeight)
-      imgHeight = MAXHEIGHT
-    
-    image = image.resize((imgWidth, imgHeight), Image.BILINEAR)
-    print("after resize: {} {}".format(imgWidth, imgHeight))
-
-    """
-    # 裁切長型圖片
-    if imgHeight > imgWidth :
-      image = image.crop((0, 0, imgWidth, imgWidth))  # (left, upper, right, lower)
-      imgWidth, imgHeight = image.size
-      print("after crop: {} {}".format(imgWidth, imgHeight))
-    """
-
-    # Reducing Quality
-    #quality_val = 90
-
-    #image.show()
-    image.save(outpath, "png")
-    
+    reduce_and_save_image(res.content, outpath)
     sleep(1)
 
 end_time = datetime.datetime.now()
